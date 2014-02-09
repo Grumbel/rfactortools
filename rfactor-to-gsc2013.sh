c48f75a874126e8df56e33bb4b36e69d28182bff
Ingo Ruhnke <grumbel@gmail.com>
2014-02-09 20:36:21 +0100

    Use maspack.py/masunpack.py and overwrite .mas files in place

--- rfactor-to-gsc2013.sh
+++ rfactor-to-gsc2013.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-mas2files="./MAS2Files.exe"
-files2mas="./Files2MAS.exe"
+mas2files="./masunpack.py"
+files2mas="./maspack.py"
 
 for MASFILE in "$@"; do
   TMPDIR="/tmp/rfactor-$(uuidgen)/"
 
   "${mas2files}" "${MASFILE}" "${TMPDIR}"
   find "${TMPDIR}" -type f -exec ./rfactordec -s 4b1dca9f960524e8 -e -o {} {} \;
-  "${files2mas}" "${TMPDIR}" "${MASFILE}.new"
+  "${files2mas}" "${TMPDIR}" "${MASFILE}"
   rm -rfv "$TMPDIR"
 done
 
