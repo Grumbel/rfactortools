7802b19f2974df3585f236a220b6a34fddeb35ee
Ingo Ruhnke <grumbel@gmail.com>
2014-03-02 03:19:20 +0100

    Minor cleanup

--- masunpack.py
+++ masunpack.py
@@ -16,10 +16,6 @@
 ##  You should have received a copy of the GNU General Public License
 ##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
-import struct
-import zlib
-import os
 import argparse
 
 import rfactortools
