b9ea5601798591b9270408a47c2a209026772c07
Ingo Ruhnke <grumbel@gmail.com>
2014-03-05 01:31:53 +0100

    Added function for case insensitive file lookup

--- rfactortools/util.py
+++ rfactortools/util.py
@@ -14,7 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 import os
+import pathlib
 
 
 def find_files(directory, ext=None):
@@ -27,5 +29,37 @@ def find_files(directory, ext=None):
                 results.append(os.path.join(path, fname))
 
     return results
+
+
+def _lookup_path_icase(root, rest):
+    """
+    Given a root directory and a list of directory parts, find all
+    path that match root + rest, while ignoring the case of the filenames.
+    """
+    if not rest:
+        return [os.path.normpath(root)]
+    else:
+        results = []
+        if os.path.isdir(root):
+            for f in os.listdir(root):
+                if f.lower() == rest[0].lower():
+                    p = os.path.join(root, f)
+                    results += _lookup_path_icase(p, rest[1:])
+        return results
+
+
+def lookup_path_icase(filename):
+    """Returns list of files matching filename in a case insensitive manner"""
+
+    assert filename, "filename must not be empty: %s" % filename
+
+    filename = os.path.normpath(filename)
+    path = pathlib.Path(filename)
+    parts = path.parts
+
+    if not path.is_absolute():
+        return _lookup_path_icase(os.curdir, parts)
+    else:
+        return _lookup_path_icase(parts[0], parts[1:])
 
 # EOF #
