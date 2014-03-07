0e077fb96c0ddb6c914fc73db648ff90ce32df6d
Ingo Ruhnke <grumbel@gmail.com>
2014-03-07 18:58:48 +0100

    Strip win32 binaries on build

--- build_win32.sh
+++ build_win32.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-i586-mingw32msvc-gcc -mdll -std=c99 _crypt.c -o rfactortools/_crypt.pyd \
+i586-mingw32msvc-gcc -s -mdll -std=c99 _crypt.c -o rfactortools/_crypt.pyd \
   -Iexternal/win32/Python34/include/ \
   -Lexternal/win32/Python34/libs/ \
   -lpython34
