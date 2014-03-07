a876a57178542669c2c485b5bdab68291e84097c
Ingo Ruhnke <grumbel@gmail.com>
2014-03-07 17:32:51 +0100

    Added Win32 build script

---
+++ build_win32.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+i586-mingw32msvc-gcc -mdll -std=c99 _crypt.c -o rfactortools/_crypt.pyd \
+  -Iexternal/win32/Python34/include/ \
+  -Lexternal/win32/Python34/libs/ \
+  -lpython34
+
+# EOF #
