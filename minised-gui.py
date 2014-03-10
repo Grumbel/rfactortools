ebaffc0c724245d8a9e847829bbe155233878131
Ingo Ruhnke <grumbel@gmail.com>
2014-03-10 22:29:27 +0100

    Added ability to apply changes to files to minised

--- minised-gui.py
+++ minised-gui.py
@@ -35,7 +35,7 @@ def do_ask_directory(directory):
         directory.set(d)
 
 
-def minised_on_lines(lines, pattern, replacement, ignore_case, only_replaced_lines=False):
+def minised_on_lines(lines, pattern, replacement, ignore_case):
     """Performes search&replace on a list of strings
 
     Returns list of lines, in cases where a replacement is done it
@@ -63,10 +63,10 @@ def minised_on_lines(lines, pattern, replacement, ignore_case, only_replaced_lin
             else:
                 result.append((left, middle, middle, right))
         else:
-            if not only_replaced_lines:
-                result.append(line)
+            result.append(line)
 
     return result
+
 
 class MiniSedConfig:
     def __init__(self):
@@ -81,6 +81,7 @@ class MiniSedConfig:
         self.mark_replacements = BooleanVar(value=True)
         self.create_backups = BooleanVar(value=False)
 
+
 class MiniSedGUI(Frame):
 
     def __init__(self, master=None):
@@ -93,6 +94,17 @@ class MiniSedGUI(Frame):
 
         self.createWidgets()
         self.pack(anchor=CENTER, fill=BOTH, expand=1)
+
+        self.files_with_content = []
+
+        self.cfg.directory.trace('w', self.disable_apply)
+        self.cfg.glob.trace('w', self.disable_apply)
+        self.cfg.search.trace('w', self.disable_apply)
+        self.cfg.replace.trace('w', self.disable_apply)
+        self.cfg.ignore_case.trace('w', self.disable_apply)
+
+    def disable_apply(self, *args):
+        self.run_btn["state"] = 'disabled'        
 
     def createWidgets(self):
 
@@ -158,13 +170,14 @@ class MiniSedGUI(Frame):
         self.scrollbar = Scrollbar(self.text_frame)
         self.scrollbar.grid(column=1, row=0, sticky=N + S)
 
-        self.text = Text(self.text_frame, yscrollcommand=self.scrollbar.set, width=1, height=1)
+        self.text = Text(self.text_frame, yscrollcommand=self.scrollbar.set, width=120, height=20)
 
-        self.text.tag_config("file", background="lightgrey", foreground="black")
+        self.text.tag_config("file", background="lightgray", foreground="black")
         self.text.tag_config("search", background="lightblue", foreground="black")
         self.text.tag_config("replace", background="orange", foreground="black")
+        self.text.tag_config("hollow", foreground="gray")
+        self.text.tag_config("highlight", background="lightyellow")
 
-        self.text.insert(END, "place holder")
         self.text.config(state=DISABLED)
         self.text.grid(column=0, row=0, sticky=N + S + W + E)
         self.scrollbar.config(command=self.text.yview)
@@ -195,6 +208,7 @@ class MiniSedGUI(Frame):
         self.text.delete("1.0", END)
         self.text.config(state=DISABLED)
 
+        self.files_with_content = []
         for path, dirs, files in os.walk(directory):
             for fname in files:
                 filename = os.path.join(path, fname)
@@ -203,29 +217,46 @@ class MiniSedGUI(Frame):
                     with open(filename, 'rt', encoding='latin-1') as fin:
                         lines = fin.read().splitlines()
                     lines = minised_on_lines(lines, self.cfg.search.get(), self.cfg.replace.get(),
-                                             self.cfg.ignore_case.get(), True)
+                                             self.cfg.ignore_case.get())
+                    self.files_with_content.append((filename, lines))
 
                     self.text.config(state=NORMAL)
                     self.text.insert(END, "%s:\n" % filename, "file")
                     for line in lines:
                         if isinstance(line, tuple):
-                            self.text.insert(END, "%s" % line[0])
+                            self.text.insert(END, "%s" % line[0], "hollow")
                             self.text.insert(END, "%s" % line[1], "search")
-                            self.text.insert(END, "%s\n" % line[3])
+                            self.text.insert(END, "%s\n" % line[3], "hollow")
 
-                            self.text.insert(END, "%s" % line[0])
+                            self.text.insert(END, "%s" % line[0], "highlight")
                             self.text.insert(END, "%s" % line[2], "replace")
-                            self.text.insert(END, "%s\n" % line[3])
-                        else:
-                            self.text.insert(END, "  %s\n" % line)
+                            self.text.insert(END, "%s" % line[3], "highlight")
+                            self.text.insert(END, "\n")
+                        elif self.cfg.show_full_content.get():
+                            self.text.insert(END, "%s\n" % line)
                     self.text.insert(END, "\n")
                     self.text.config(state=DISABLED)
 
                     self.master.update()
+        self.run_btn["state"] = 'normal'
 
     def do_execute(self):
-        pass
+        for filename, lines in self.files_with_content:
+            if self.cfg.create_backups.get():
+                os.rename(filename, filename + "~")
 
+            with open(filename, 'wt', encoding='latin-1', newline='\r\n') as fout:
+                for line in lines:
+                    if isinstance(line, tuple):
+                        fout.write(line[0])
+                        fout.write(line[2])
+                        fout.write(line[3])
+                    else:
+                        fout.write(line)
+                    fout.write("\n")
+
+        self.run_btn["state"] = 'disabled'
+        tkinter.messagebox.showinfo("Replacement successful", "Replacements have been performed successfully on the files")
 
 def main():
     parser = argparse.ArgumentParser(description="A minimal sed-like tool")
