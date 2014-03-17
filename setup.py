3dcc659d99c75c4ebf9d8905a7c24b3b5b007fd0
Ingo Ruhnke <grumbel@gmail.com>
2014-03-17 23:22:07 +0100

    Fixed Copyright date from 2013 to 2014

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # rFactorTools
-# Copyright (C) 2013 Ingo Ruhnke <grumbel@gmail.com>
+# Copyright (C) 2014 Ingo Ruhnke <grumbel@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
